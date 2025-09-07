92db811bea46b4bd0afa4a6f01bcc98603019405
Thomas Mulhern <mulhern.thomasr@gmail.com>
2025-09-07 07:41:22 -0500

    clean up repo

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install dependencies
-cd /Users/thomasmulhern/projects/AI_CFO
-npm install
-
-# Build the project
-npm run build
--- make_executable.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Make all scripts executable
-chmod +x *.sh
-echo "✅ All scripts are now executable"
